0204312ce235fd7ce91d32eceb2ba9e28bb8eaea
Paul Quiroz <pquiroz1977@gmail.com>
2018-07-10 13:52:05 -0700

    events backend and sql files created

--- db/init.sql
+++ db/init.sql
@@ -33,4 +33,23 @@ CREATE TABLE events (
     group_id INTEGER References groups(id)
 );
 -------- Events Sql statements.
+-- Create new event:
+INSERT INTO events (event_name, event_topic, event_date, event_location, event_attendee_list, group_id)
+VALUES
+(${event_name}, ${event_topic}, ${event_date}, ${event_location}, ${event_attendee_list}::JSONB[], ${group_id})
+RETURNING *;
 
+-- Update event:
+UPDATE events SET (event_name, event_topic, event_date, event_location, event_attendee_list, group_id )
+VALUE
+(${event_name}, ${event_topic}, ${event_date}, ${event_location}, ${event_attendee_list}::JSONB[], ${group_id});
+SELECT * FROM events;
+
+-- Delete event:
+DELETE FROM events WHERE id = $1;
+SELECT * FROM events;
+
+-- Read all events:
+SELECT * FROM events;
+
+ 

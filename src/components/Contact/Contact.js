9ebdd0d9a47b132fc9dea8306ed2bb4d30712ba5
Bryson <bryce.akau08@gmail.com>
2018-07-11 16:58:53 -0700

    nodemailer in backend

--- src/components/Contact/Contact.js
+++ src/components/Contact/Contact.js
@@ -1,13 +1,55 @@
 import React, { Component } from 'react';
+import axios from 'axios'
 
 export default class  extends Component {
     constructor(){
         super()
+        this.state = {
+            name: '',
+            email: '',
+            text: ''
+        }
+        this.handleName = this.handleName.bind(this)
+        this.handleEmail = this.handleEmail.bind(this)
+        this.handleText = this.handleText.bind(this)
+        this.handleSubmitButton = this.handleSubmitButton(this)
     }
+
+    handleName(e){
+        this.setState({
+            name: e
+        })
+    }
+
+    handleEmail(e){
+        this.setState({
+            email: e
+        })
+    }
+
+    handleText(e){
+        this.setState({
+            text: e
+        })
+    }
+
+    handleSubmitButton(e){
+        const {name, email, text } = this.state
+        axios.post('/api/contact', {
+            name,
+            email,
+            text
+        }).then(response => {
+            console.log('=======>response', response)
+        })
+    }
+
     render() {
         return (
             <div>
-                
+                <form action="/contact" id="contact-form" method="post" role="form">
+                </form>
+
             </div>
         );
     }

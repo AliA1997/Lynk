15153ec18e629e9a6a44a43c8b362e3bff61d87e
Bryson <bryce.akau08@gmail.com>
2018-07-11 11:21:48 -0700

    committing

--- server/index.js
+++ server/index.js
@@ -2,29 +2,30 @@
 require('dotenv').config();
 //Initializes the server
 const express = require('express');
-//
+//Installing Nodemailer
+const nodemailer = require('nodemailer');
 //Middlewares   
 //Initiallizes the req.body, so without it, the req.body will not be defined.
 const bodyParser = require('body-parser');
 //Initializes the session
 const session = require('express-session');
-//
+
 //Connect to database with the connection string from your .env file.
 //And configure your server to it.
 massive(process.env.CONNECTION_STRING).then(database => {
     //Then assign the database, directory of the sql files to the server.
     app.set('db', database);
 }).catch(err => console.log(err, 'Database Connection Error'))
-//
+
 //Controller Files 
 const user = require("./controllers/user_controller");
 
-//
+
 
 
 ///Define the server 
 const app =  express();
-//
+
 
 //User Endpoints 
 app.post('/api/login', user.login);

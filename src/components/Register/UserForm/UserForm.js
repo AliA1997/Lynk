fb6f388d2f7a6e4779c3ba12b2b382493b5b546c
AliA1997 <devmtnali@gmail.com>
2018-07-12 14:45:48 -0700

    Fuck

--- src/components/Register/UserForm/UserForm.js
+++ src/components/Register/UserForm/UserForm.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import TextField from '@material-ui/core/TextField';
-
 import Avatar from '@material-ui/core/Avatar';
 import Button from '@material-ui/core/Button';
 import './UserForm.css';

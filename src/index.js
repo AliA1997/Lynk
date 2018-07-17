8cce8995fd58fb1f3b190f600ae436e21b134d87
AliA1997-Medium <devmtnali@gmail.com>
2018-07-16 23:40:09 -0700

    Completed Redux-Persist

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import { BrowserRouter as R } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './ducks/store';
+import { PersistGate } from 'redux-persist/integration/react';
+import { store, persistor } from './ducks/store';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Provider store={store}><R><App /></R></Provider>, document.getElementById('root'));
+ReactDOM.render(<R><Provider store={store}><PersistGate persistor={persistor}><App /></PersistGate></Provider></R>,
+     document.getElementById('root'));
 registerServiceWorker();

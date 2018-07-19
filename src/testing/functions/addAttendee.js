f047ab1b4b6405c919fee545389e93a77ecb0efa
Paul Quiroz <pquiroz1977@gmail.com>
2018-07-19 14:01:47 -0700

    jest testing

--- src/testing/functions/addAttendee.js
+++ src/testing/functions/addAttendee.js
@@ -1,3 +1,30 @@
 module.exports = {
-    
+
+    event_attendees:[
+        {
+            id: 1,
+            name: 'Paul'
+        },
+        {
+            id: 2,
+            name: 'Ali'
+        },
+        {
+            id: 3,
+            name: 'Bryson'
+        },
+        {
+            id: 4,
+            name: 'Brent'
+        }
+    ],
+
+    addAttendee(){
+        this.event_attendees.push({
+            id: 5,
+            name: 'Anthony'
+        }) 
+        return this.event_attendees;
+    }
 }
+
--- src/testing/functions/addGroup.js
+++ src/testing/functions/addGroup.js
@@ -1,3 +1,24 @@
 module.exports = {
-    
+    groupList: [
+        {
+            id: 1,
+            name: 'Wildcat group'
+        },
+        {
+            id: 2,
+            name: 'Suns group'
+        },
+        {
+            id: 3,
+            name: 'Dbacks group'
+        }
+    ],
+
+    addGroup(){
+        this.groupList.push({
+            id: 4,
+            name: 'Cards group'
+        })
+        return this.groupList;
+    }
 }

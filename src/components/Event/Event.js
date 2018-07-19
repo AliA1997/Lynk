06af845f05b5699048e9d7f7dad81894718248bc
AliA1997-Medium <devmtnali@gmail.com>
2018-07-19 14:35:00 -0700

    Completed Event Page

---
+++ src/components/Event/Event.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+import './Event.csså';
+
+export default class Event extends Component {
+    render() {
+        return (
+            <div>
+            </div>
+        );
+    }
+}

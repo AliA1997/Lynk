e1212be4d3cdc0d7c81ac32db2eaf239fbe7c2fd
AliA1997-Medium <devmtnali@gmail.com>
2018-07-21 21:48:52 -0700

    COmpleted Dashbaord

--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,9 +1,16 @@
 import React, { Component } from 'react';
-import './Dashboard.css'
+import placeholderImage from '../../Images/default-placeholder.png';
 import Groups from '../Groups/Groups';
+//Material-UI Components
+import Avatar from '@material-ui/core/Avatar';
+import Typography from '@material-ui/core/Typography';
+//React-icons
+import IoIosCogOutline from 'react-icons/lib/io/ios-cog-outline';
+import { connect } from 'react-redux';
+import './Dashboard.css'
 
 ///Dashboard components.
-export default class Dashboard extends Component {
+class Dashboard extends Component {
     constructor() {
         super();
         this.state = {
@@ -22,18 +29,34 @@ export default class Dashboard extends Component {
 
     
     render() {
-        
+        const { user } = this.props;
         return (
             <div className='dashboard-className'>
-                <h1>Dashboard</h1>
-                <div>
-                    <div className='dashboard-event-form-div'>
+                <Typography variant='outlined' style={{textAlign: 'center'}}>Dashboard</Typography>
+                <hr />
+                <div className='dashboard-user-container-div'>
+                    <div className='dashboard-user-div'>
+                        <Avatar style={{height: '15em', width: '15em'}}
+                        src={user.profile_picture || placeholderImage} alt={user.username + ' picture!'} />
+                        {user.username && <Typography variant='outlined'>{user.username}</Typography>}
+                        <Typography variant='outlined'>{user.email}</Typography>
+                        <Typography variant='outlined'>{user.name}</Typography>
+                        <Typography variant='outlined'>
+                            # Groups a user is part of: {user.groups && user.groups.length}
+                        </Typography>
+                        <Typography variant='outlined'>
+                            # Events a user has went to: {user.events && user.events.length}
+                        </Typography>
                     </div>
-                    <div className='dashboard-group-form-div'>
-                        
-                    </div>
+                    <hr />
+                    <IoIosCogOutline style={{fontSize: '5em', widhr: '100%'}} color='primary'/>
                 </div>
-                <Groups isDashboard={true} />
+                <Typography><b>Groups you are part of.</b></Typography>
+                <hr />
+                <Groups isDashboard={true} groupsUserPartOf={true} />
+                <Typography><b>Groups you run</b></Typography>
+                <hr />
+                <Groups isDashboard={true} groupsUserAdmin={true} />
                 <div>
                     <button>
 
@@ -43,4 +66,12 @@ export default class Dashboard extends Component {
         );
     }
 }
+
+const mapStateToProps = state => {
+    return {
+        user: state.user 
+    };
+}
+
+export default connect(mapStateToProps)(Dashboard);
 
--- src/components/Groups/Groups.js
+++ src/components/Groups/Groups.js
@@ -3,6 +3,7 @@ import GroupCard from './GroupCard/GroupCard';
 import axios from 'axios';
 import { connect } from 'react-redux';
 import Loading from '../Global/Loading/Loading';
+import { Typography } from '../../../node_modules/@material-ui/core';
 
 class Groups extends Component {
     constructor(){
@@ -14,6 +15,7 @@ class Groups extends Component {
     }
 
     componentDidMount() {
+        let arr = [];
         //Axios call setting state with groups array
         //If there is no isDashboard prop.
         if(!this.props.isDashboard) {
@@ -21,19 +23,36 @@ class Groups extends Component {
                 //Gets all default groups
                 this.setState({groups: res.data.groups, loading: false})
             }).catch(err => console.log('Get Groups Error-------------', err));
-        } else {
+        } else if(this.props.isDashboard && this.props.groupsUserAdmin) {
             //Destructiong the user
             const { user } = this.props;
             axios.get(`/api/groups/admin/${user.id}`).then(res => {
+                //Only display five groups.
+                for(let i = 0; i < 5; i++) {
+                    if(res.data.groups[i]) arr.push(res.data.groups[i]);
+                }
                 //Get all groups that the user is in charge of.
-                this.setState({groups: res.data.groups, loading: false});
+                this.setState({groups: arr, loading: false});
             }).catch(err => console.log('Get User Admin Groups------------', err));
+        } else if(this.props.isDashboard && this.props.groupsUserPartOf) {
+            //Destruct the user
+            const { user } = this.props;
+            //Get the groups the user is a part of based on the props.
+            axios.get(`/api/groups/user/${user.id}`).then(res => {
+                //Only display five groups.
+                for(let i = 0; i < 5; i++) {
+                    if(res.data.groups[i]) arr.push(res.data.groups[i]);
+                }
+                //Get all the groups .
+                this.setState({groups: arr, loading: false});
+            }).catch(err => console.log('Get User Groups------------', err));
         }
     }
     reRenderGroups = groups => {
         this.setState({groups})
     }
     render() {
+
         //Destructuring groups from this.state
         const{ groups, loading } = this.state;
         if(!loading) {
@@ -42,7 +61,9 @@ class Groups extends Component {
                     {this.props.isDashboard ? <h1>Groups in Charge</h1> : <h1>Groups</h1>}
                     <div>
                 {/* Mapping over groups array and returning GroupCard with spread operator passing each property of group*/}
-                        {groups.length && groups.map((group, i) => <GroupCard key={i} {...group} isDashboard={this.props.isDashboard} reRenderGroups={this.reRenderGroups}/>)}
+                        {groups.length ? 
+                        groups.map((group, i) => <GroupCard key={i} {...group} isDashboard={this.props.isDashboard} reRenderGroups={this.reRenderGroups}/>)
+                        : <Typography>Sorry they are no groups........ &#x1F622;</Typography>}
                     </div>
                 </div>
             );
@@ -53,7 +74,9 @@ class Groups extends Component {
 }
 //Define the default props.
 Groups.defaultProps = {
-    isDashboard: false
+    isDashboard: false,
+    groupsUserPartOf: false,
+    groupsUserAdmin: false
 }
 
 const mapStateToProps = state => {
